3ef3792cc71f3b3bcddfea9256a2da39651c12be
Eoin McCarthy <eoinmcc@gmail.com>
2018-03-12 09:07:15 +1000

    fixes

--- transform_stream.py
+++ transform_stream.py
@@ -67,7 +67,7 @@ def main():
         dest_m3u8_obj = m3u8.load(os.path.join(dest_m3u_filepath,'test.m3u8'))  # this could also be an absolute filename
         dest_segment_uris = set([x.uri for x in src_m3u8_ojb.segments])
     else:
-        dest_m3u8_obj = set()
+        dest_segment_uris = set()
 
     segment_uris_to_transcode = src_segment_uris.difference(dest_segment_uris)
 
