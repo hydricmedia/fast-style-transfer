8c3bf752e65015a7b387b8783fb9dbe13b688648
Eoin McCarthy <eoinmcc@gmail.com>
2018-03-12 09:04:38 +1000

    fixes

--- transform_stream.py
+++ transform_stream.py
@@ -54,14 +54,14 @@ def main():
     import m3u8
 
     #1 get the src and dest m3u8 files  - which files need to be transcoded
-    if os.path.isfile(os.join(dest_m3u_filepath,'test.m3u8')):
-        src_m3u8_obj = m3u8.load(os.join(src_m3u_filepath,'test.m3u8'))  # this could also be an absolute filename
+    if os.path.isfile(os.path.join(dest_m3u_filepath,'test.m3u8')):
+        src_m3u8_obj = m3u8.load(os.path.join(src_m3u_filepath,'test.m3u8'))  # this could also be an absolute filename
         src_segment_uris = set([x.uri for x in src_m3u8_ojb.segments])
     else:
         src_segment_uris = set()
     import os.path
-    if os.path.isfile(os.join(dest_m3u_filepath,'test.m3u8')):
-        dest_m3u8_obj = m3u8.load(os.join(dest_m3u_filepath,'test.m3u8'))  # this could also be an absolute filename
+    if os.path.isfile(os.path.join(dest_m3u_filepath,'test.m3u8')):
+        dest_m3u8_obj = m3u8.load(os.path.join(dest_m3u_filepath,'test.m3u8'))  # this could also be an absolute filename
         dest_segment_uris = set([x.uri for x in src_m3u8_ojb.segments])
     else:
         dest_m3u8_obj = set()
@@ -77,12 +77,12 @@ def main():
 
     #3 transcode the segments which haven't already been transcoded
     for segment_uri in segment_uris_to_transcode:
-        evaluate.ffwd_video(os.join(src_m3u_filepath, segment_uri), os.join(dest_m3u_filepath, segment_uri), opts.checkpoint, opts.device, opts.batch_size)
+        evaluate.ffwd_video(os.path.join(src_m3u_filepath, segment_uri), os.path.join(dest_m3u_filepath, segment_uri), opts.checkpoint, opts.device, opts.batch_size)
 
     if len(segment_uris_to_transcode) > 0:
         #4 copy across m3u8 file
         import shutil
-        shutil.copy(src_m3u_filepath, dest_m3u_filepath)
+        shutil.copy(os.path.join(src_m3u_filepath,'test.m3u8'), os.path.join(dest_m3u_filepath,'test.m3u8'))
 
     import time
     time.sleep()
